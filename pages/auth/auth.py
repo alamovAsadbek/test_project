04d3fbc00c98183fa614d47d8559cf4c15f5b9cb
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-01 14:23:48 +0500

    Change create_user_table func in auth.py

--- pages/auth/auth.py
+++ pages/auth/auth.py
@@ -1,7 +1,20 @@
+from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
 class Auth:
     @log_decorator
     def create_user_table(self):
-        pass
+        query = '''
+        CREATE TABLE IF NOT EXISTS users (
+        ID SERIAL PRIMARY KEY,
+        FIRST_NAME VARCHAR(255) NOT NULL,
+        LAST_NAME VARCHAR(255) NOT NULL,
+        USERNAME VARCHAR(255) NOT NULL,
+        PASSWORD VARCHAR(255) NOT NULL,
+        IS_LOGIN BOOLEAN DEFAULT FALSE,
+        CREATED TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        )
+        '''
+        execute_query(query)
+        return True
