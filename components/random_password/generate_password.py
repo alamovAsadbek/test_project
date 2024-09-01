2d0c6937685d2f4f5d0798691524a4ecea16ee68
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-01 14:15:59 +0500

    Changa code in generate_password.py

---
+++ components/random_password/generate_password.py
@@ -0,0 +1,9 @@
+from random import random
+
+from main_files.decorator.decorator_func import log_decorator
+
+
+@log_decorator
+def generate_password() -> int:
+    random_num = random.randint(1000, 999999)
+    return random_num
