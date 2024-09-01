071b2044b032459eb34e48dd9e3a53f709576111
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-01 12:23:58 +0500

    Change auth_menu func

--- main.py
+++ main.py
@@ -3,4 +3,14 @@ from main_files.decorator.decorator_func import log_decorator
 
 @log_decorator
 def auth_menu():
-    pass
+    text = '''
+1. Register
+2. Login
+3. Logout
+    '''
+    print(text)
+    try:
+        pass
+    except Exception as e:
+        print(f'Error: {e}')
+        auth_menu()
