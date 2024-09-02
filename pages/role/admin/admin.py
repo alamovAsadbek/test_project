a7bfab17d6a6c4d1920f579004a8bfaa80ec7d51
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-02 14:10:41 +0500

    Change __init__ func in admin.pt

--- pages/role/admin/admin.py
+++ pages/role/admin/admin.py
@@ -4,7 +4,7 @@ from main_files.decorator.decorator_func import log_decorator
 
 class Admin:
     def __init__(self):
-        self.__pagination_users = Pagination('users')
+        self.__pagination_users = Pagination('users', ['id, first_name, last_name, username, created_at'])
 
     @log_decorator
     def show_all_users(self):
