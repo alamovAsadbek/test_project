e198d70216e3df4f89d8d6158a97f2aaa376c0bb
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-02 13:25:08 +0500

    Create __init__ func in admin.py

--- pages/role/admin/admin.py
+++ pages/role/admin/admin.py
@@ -1,7 +1,11 @@
+from components.pagination.pagination import Pagination
 from main_files.decorator.decorator_func import log_decorator
 
 
 class Admin:
+    def __init__(self):
+        self.__pagination = Pagination()
+
     @log_decorator
     def show_all_users(self):
         pass
