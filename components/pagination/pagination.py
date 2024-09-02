6212f545e4cace08a89a2f52fbf9ae0b55f19be0
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-02 13:19:23 +0500

    Change read_table func in pagination.py

--- components/pagination/pagination.py
+++ components/pagination/pagination.py
@@ -1,9 +1,12 @@
+from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
 class Pagination:
     def __init__(self, table_name):
         self.table_name = table_name
+
     @log_decorator
     def read_table(self):
-        pass
+        query = "SELECT * FROM {}".format(self.table_name)
+        return execute_query(query, fetch='all')
