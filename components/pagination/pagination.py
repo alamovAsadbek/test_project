7e9d259936970a7eda09f67e9a11045aa6b76d7f
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-02 13:18:09 +0500

    Create read_table func in pagination.py

--- components/pagination/pagination.py
+++ components/pagination/pagination.py
@@ -1,3 +1,9 @@
+from main_files.decorator.decorator_func import log_decorator
+
+
 class Pagination:
     def __init__(self, table_name):
         self.table_name = table_name
+    @log_decorator
+    def read_table(self):
+        pass
