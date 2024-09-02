b53b444b04f2339f4ec459e4da214c9e32a5a042
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-02 19:40:59 +0500

    Change update_user func in admin.py

--- pages/role/admin/admin.py
+++ pages/role/admin/admin.py
@@ -1,3 +1,6 @@
+import hashlib
+import threading
+
 from components.pagination.pagination import Pagination
 from main_files.database.db_setting import execute_query
 from main_files.decorator.decorator_func import log_decorator
@@ -10,6 +13,7 @@ class Admin:
 
     @log_decorator
     def show_all_users(self):
+        print("Waiting...")
         self.__pagination_users.page_tab()
         return True
 
@@ -26,5 +30,18 @@ class Admin:
     @log_decorator
     def update_user(self):
         user_id: int = int(input("Enter user id to update: "))
+        print("Waiting...")
         get_data = self.get_data(table_name='users', data_id=user_id)
-        print(get_data)
+        if get_data is None:
+            print("User not found")
+            return False
+        print(f"\nID: {get_data['id']}\nFirst name: {get_data['first_name']}\nLast name: {get_data['last_name']}\n"
+              f"Username: {get_data['username']}\n")
+        new_password = hashlib.md5(input("Enter new password: ").strip().encode('utf-8')).hexdigest()
+        query = '''
+        UPDATE users SET password=%s WHERE ID=%s;
+        '''
+        params = (new_password, user_id)
+        threading.Thread(target=execute_query, args=(query, params)).start()
+        print("Updated successfully")
+        return True
