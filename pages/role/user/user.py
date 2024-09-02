8b6ed684c0d825f0e96ec234c6ab4e8ae71e1c46
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-02 22:28:28 +0500

    Change create_test func in user.py

--- pages/role/user/user.py
+++ pages/role/user/user.py
@@ -1,10 +1,12 @@
 from main_files.decorator.decorator_func import log_decorator
+from pages.role.user.test import Test
 
 
 class User:
     def __init__(self):
-        pass
+        self.__test = Test()
 
     @log_decorator
     def create_test(self):
-        pass
+        self.__test.create_test()
+        return self.__test
