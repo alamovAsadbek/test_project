5200b154a7a87f947a5bdd9ca7ff5d1b98bb1964
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-02 21:31:38 +0500

    Change create_test func in test.py

--- pages/role/user/test.py
+++ pages/role/user/test.py
@@ -1,5 +1,5 @@
 from components.random_password.generate_password import generate_password
-from main_files.database.db_setting import get_active_user
+from main_files.database.db_setting import get_active_user, execute_query
 from main_files.decorator.decorator_func import log_decorator
 
 
@@ -9,7 +9,12 @@ class Test:
 
     @log_decorator
     def question_func(self):
-        pass
+        question_name: str = input("Enter your question: ")
+        query = '''
+        INSERT INTO questions (name) VALUES (%s)
+        RETURNING id
+        '''
+        params = (question_name,)
 
     @log_decorator
     def create_test(self):
@@ -18,3 +23,10 @@ class Test:
         number_of_answers: int = int(input("Enter number of answers: ").strip())
         test_id = generate_password()
         print(f"Your test id: {test_id}")
+        query = '''
+        INSERT INTO tests (name, test_id) VALUES (%s, %s)
+        RETURNING id
+        '''
+        params = (test_name, test_id)
+        result = execute_query(query, params)
+        print(result)
