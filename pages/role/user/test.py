1c903e2b9b88f0cc038c5d5d65babaae4f71e5bb
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-02 20:25:36 +0500

    Create question_func func in test.py

--- pages/role/user/test.py
+++ pages/role/user/test.py
@@ -8,10 +8,13 @@ class Test:
         self.__active_user = get_active_user()
 
     @log_decorator
+    def question_func(self):
+        pass
+
+    @log_decorator
     def create_test(self):
         test_name: str = input("Enter test name: ").strip()
         number_of_questions: int = int(input("Enter number of questions: ").strip())
         number_of_answers: int = int(input("Enter number of answers: ").strip())
         test_id = generate_password()
         print(f"Your test id: {test_id}")
-        
