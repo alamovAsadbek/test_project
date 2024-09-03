3c966cead3a69ea3a93927c6550a5929d9572291
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-03 22:50:29 +0500

    change view_statistics_on_questions func in test.py

--- pages/role/user/test.py
+++ pages/role/user/test.py
@@ -615,4 +615,4 @@ class Test:
                     print(Fore.GREEN + f'\t\t{index_opt + 1}: {option["name"]}')
                     continue
                 print(f'\t\t{index_opt + 1}: {option["name"]}')
-                print(f"\nCorrect answers: {result_true[0]}\tWrong answers: {result_false[0]}\n")
+            print(f"\nCorrect answers: {result_true[0]}\tWrong answers: {result_false[0]}\n")
