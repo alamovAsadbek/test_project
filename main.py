a0eadcc9eb2e075ac1cf744873edfe3f1302bc10
Asadbek A'lamov <104184218+alamovAsadbek@users.noreply.github.com>
2024-09-03 18:00:16 +0500

    Chnage statistics_menu func in main.py

--- main.py
+++ main.py
@@ -178,7 +178,7 @@ def statistics_menu():
         user_input: int = int(input("Choose menu: "))
         if user_input == 1:
             print("\n<-\t Home / Statistics / View statistics on tests\t ->\n")
-            pass
+            user.statistics_test()
         elif user_input == 2:
             user_menu()
         else:
